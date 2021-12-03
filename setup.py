db6093018e14523ad31975c2cae4b035bc963ce8
Caitlin Gainey <caitlingainey@MacBook-Pro-4.local>
2021-12-03 13:21:13 -0500

    changing setup.py

--- setup.py
+++ setup.py
@@ -7,10 +7,7 @@ setuptools.setup(
     author="Caitlin Gainey",
     author_email="caitlin.gainey@yale.edu",
     description="A map generating and plotting application for CO isotopologue data, specifically 3D cubes",
-    #packages=setuptools.find_packages(where="src"),
-    #packages=["exampy","exampy/plotting"]
     packages=setuptools.find_packages(include=['astrocart','astrocart/*']),
-    package_dir={"": "src"},
     python_requires='>=3',
     install_requires=["numpy", "matplotlib", "astropy", "spectral_cube", "reproject"]
 )
